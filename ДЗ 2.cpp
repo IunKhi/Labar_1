59bfd964b53be3565b0f7f34229108e46b302768
IunKhi <145861160+IunKhi@users.noreply.github.com>
2023-09-25 22:57:46 +0300

    Update ДЗ 2.cpp

--- ДЗ 2.cpp
+++ ДЗ 2.cpp
@@ -7,3 +7,28 @@
 6) push_back - std::string::push_back()
 7) pop_back - std::string::pop_back()
 */
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main() {
+    std::string input;
+    std::cout << "Введите строку: ";
+    std::getline(std::cin, input);
+
+    // Удаление пробелов и пунктуационных знаков
+    std::string cleanInput;
+    for (char c : input) {
+        if (std::isalpha(c)) {
+            cleanInput.push_back(std::tolower(c));
+        }
+    }
+
+    // Сортировка букв в алфавитном порядке
+    std::sort(cleanInput.begin(), cleanInput.end());
+
+    std::cout << "Строка после сортировки: " << cleanInput << std::endl;
+
+    return 0;
+}
